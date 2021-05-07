b45eb8612b9a8e5cc2b02759facb4a79661aa7e4
Circe Russell <circeroserussell@gmail.com>
2021-05-07 15:00:34 -0400

    guest update is working, form needed for both updates.

--- src/Components/Site/Guests/GuestView.tsx
+++ src/Components/Site/Guests/GuestView.tsx
@@ -4,14 +4,71 @@ import Couple2 from "../../../Assets/couple2.png";
 type acceptedProps = {
   token: any;
   myGuests: any;
-  fetchGuests: Function;
+  fetchGuests: any;
 };
 
-export class GuestView extends Component<acceptedProps, {}> {
+type valueTypes = {
+  myGuests: [];
+  name: string;
+  side: string;
+  relation: string;
+  theirSpouse: string;
+  theirKids: string;
+};
+export class GuestView extends Component<acceptedProps, valueTypes> {
   constructor(props: acceptedProps) {
     super(props);
+    this.state = {
+      myGuests: [],
+      name: "",
+      side: "",
+      relation: "",
+      theirSpouse: "",
+      theirKids: "",
+    };
     console.log(this.props);
   }
+  componentDidMount() {
+    (id: any) => this.handleSubmit2(id);
+  }
+
+  handleSubmit2 = (id: number) => {
+    if (true) {
+      console.log(id);
+      fetch(`http://localhost:3000/guest/`, {
+        method: "GET",
+        headers: new Headers({
+          "Content-Type": "application/json",
+          Authorization: this.props.token,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          this.setState({ myGuests: data });
+        });
+    }
+  };
+
+  updateGuest = (id: number) => {
+    fetch(`http://localhost:3000/guest/${id}`, {
+      method: "PUT",
+      headers: new Headers({
+        "Content-Type": "application/json",
+        Authorization: this.props.token,
+      }),
+      body: JSON.stringify({
+        name: this.state.name,
+        side: this.state.side,
+        relation: this.state.relation,
+        theirSpouse: this.state.theirSpouse,
+        theirKids: this.state.theirKids,
+      }),
+    })
+      .then((res) => res.json())
+      .then((id: number) => {
+        this.handleSubmit2(id);
+      });
+  };
 
   deleteGuest = async (id: number) => {
     await fetch(`http://localhost:3000/guest/${id}`, {
@@ -39,30 +96,31 @@ export class GuestView extends Component<acceptedProps, {}> {
                     className="mx-8 my-12 w-72 rounded-1xl bg-white border shadow-md overflow-hidden"
                     key={guest.id}
                   >
-                    <p className="mt-5 text-lg text-gray-800 text-center mb-3">
+                    <div className="mt-5 text-lg text-gray-800 text-center mb-3">
                       <h1 className="font-semibold">Name:</h1>
                       {guest.name}
-                    </p>
-                    <p className="text-md text-center mb-5">
+                    </div>
+                    <div className="text-md text-center mb-5">
                       <h1 className="font-semibold">Side:</h1> {guest.side}
-                    </p>
-                    <p className="text-md text-center mb-5">
+                    </div>
+                    <div className="text-md text-center mb-5">
                       <h1 className="font-semibold">Relation to:</h1>{" "}
                       {guest.relation}
-                    </p>
-                    <p className="text-md text-center mb-5">
+                    </div>
+                    <div className="text-md text-center mb-5">
                       <h1 className="font-semibold">Spouse:</h1>
                       {guest.theirSpouse}
-                    </p>
-                    <p className="text-md text-center mb-5">
+                    </div>
+                    <div className="text-md text-center mb-5">
                       <h1 className="font-semibold">Children:</h1>{" "}
                       {guest.theirKids}
-                    </p>
+                    </div>
                     <div className="mb-2">
                       <div className="flex flex-wrap space-x-6 justify-center">
                         <button
                           type="button"
                           className="py-2 px-4  bg-indigo-400 hover:bg-indigo-700 focus:ring-pink-500 focus:ring-offset-pink-200 text-white w-1/3 transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-full"
+                          onClick={() => this.updateGuest(guest.id)}
                         >
                           Update
                         </button>
