50deea189f5ec788d27eb09e967e68bf626cd3af
Circe Russell <circeroserussell@gmail.com>
2021-05-08 21:26:21 -0400

    helpers update

--- src/helpers/environment.ts
+++ src/helpers/environment.ts
@@ -5,18 +5,18 @@ switch (window.location.hostname) {
   case "127.0.0.1":
     APIURL = "http://localhost:3000";
     break;
-  case "https://theweddingbookapp.herokuapp.com/auth":
+  case "https://the-weddingbook-app.herokuapp.com/auth":
     APIURL = "https://crr-wedding-book.herokuapp.com";
     break;
-  case "https://theweddingbookapp.herokuapp.com":
+  case "https://the-weddingbook-app.herokuapp.com":
     APIURL = "https://crr-wedding-book.herokuapp.com";
     break;
-  case "https://theweddingbookapp.herokuapp.com/":
-    APIURL = "https://crr-wedding-book.herokuapp.com";
-    break;
-  case "https://theweddingbookapp.herokuapp.com/auth":
-    APIURL = "https://crr-wedding-book.herokuapp.com";
-    break;
+  //   case "https://the-weddingbook-app.herokuapp.com/":
+  //     APIURL = "https://crr-wedding-book.herokuapp.com";
+  //     break;
+  //   case "https://the-weddingbook-app.herokuapp.com/auth":
+  //     APIURL = "https://crr-wedding-book.herokuapp.com";
+  //     break;
 }
 
 export default APIURL;
